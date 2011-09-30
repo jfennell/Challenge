804afda6e06da1af2d0189005a5e84870fa779d1
Jason Fennell <jdfennell@gmail.com>
2011-09-29 23:15:45 -0700

    Much much faster version... definitely do not need the generality of the trie

--- friends.py
+++ friends.py
@@ -253,9 +253,109 @@ def main():
 	friends = find_friend_closure('causes')
 	print 'Took %s to find friends.' % (datetime.datetime.now() - start,)
 	print 'There are %d friends.' % (len(friends),)
+
+
+
+A_TO_Z = tuple(chr(idx) for idx in xrange(97, 123))
+A_TO_Z_AND_EMPTY = tuple(list(A_TO_Z) + [''])
+def all_one_edits(word):
+	"""Generate all strings one edit from `word`."""
+	# Replace and delete
+	for i in xrange(len(word)):
+		for char in A_TO_Z_AND_EMPTY:
+			yield word[:i] + char + word[i+1:]
+
+	# Insert
+	for i in xrange(len(word)+1):
+		for char in A_TO_Z:
+			yield word[:i] + char + word[i:]
+
+
+def one_edit_find_friend_closures(start):
+	"""Factory function for a trie loaded with word.list."""
+	# Get the dictionary
+	dictionary = set()
+	with open('word.list', 'r') as f:
+		for line in f:
+			dictionary.add(line.strip())
+
+	all_friends = set([start])
+	to_expand = [start]
+
+	count = 0
+	while to_expand:
+		s = to_expand.pop(0)
+		for w in all_one_edits(s):
+			if w in dictionary and w not in all_friends:
+				to_expand.append(w)
+				all_friends.add(w)
+
+		count += 1
+		if count > 0 and count % 1000 == 0:
+			print 'Expanded %d times. %d in queue.  %d friends so far.' % (
+				count, len(to_expand), len(all_friends)
+			)
+	return all_friends
+
+
+class TestSingleEdit(unittest.TestCase):
+	def test_empty_str(self):
+		self.assertEqual(
+			set(all_one_edits('')),
+			# You don't get the empty string, since that would be 0 edits away
+			set(A_TO_Z)
+		)
+
+	def test_one_char(self):
+		self.assertEqual(
+			set(all_one_edits('X')),
+			set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+			'z', '', 'aX', 'bX', 'cX', 'dX', 'eX', 'fX', 'gX', 'hX', 'iX',
+			'jX', 'kX', 'lX', 'mX', 'nX', 'oX', 'pX', 'qX', 'rX', 'sX', 'tX',
+			'uX', 'vX', 'wX', 'xX', 'yX', 'zX', 'Xa', 'Xb', 'Xc', 'Xd', 'Xe',
+			'Xf', 'Xg', 'Xh', 'Xi', 'Xj', 'Xk', 'Xl', 'Xm', 'Xn', 'Xo', 'Xp',
+			'Xq', 'Xr', 'Xs', 'Xt', 'Xu', 'Xv', 'Xw', 'Xx', 'Xy', 'Xz'])
+		)
+
+	def test_multi_char(self):
+		self.assertEqual(
+			set(all_one_edits('XYZ')),
+			set(['aYZ', 'bYZ', 'cYZ', 'dYZ', 'eYZ', 'fYZ', 'gYZ', 'hYZ', 'iYZ',
+			'jYZ', 'kYZ', 'lYZ', 'mYZ', 'nYZ', 'oYZ', 'pYZ', 'qYZ', 'rYZ',
+			'sYZ', 'tYZ', 'uYZ', 'vYZ', 'wYZ', 'xYZ', 'yYZ', 'zYZ', 'YZ',
+			'XaZ', 'XbZ', 'XcZ', 'XdZ', 'XeZ', 'XfZ', 'XgZ', 'XhZ', 'XiZ',
+			'XjZ', 'XkZ', 'XlZ', 'XmZ', 'XnZ', 'XoZ', 'XpZ', 'XqZ', 'XrZ',
+			'XsZ', 'XtZ', 'XuZ', 'XvZ', 'XwZ', 'XxZ', 'XyZ', 'XzZ', 'XZ',
+			'XYa', 'XYb', 'XYc', 'XYd', 'XYe', 'XYf', 'XYg', 'XYh', 'XYi',
+			'XYj', 'XYk', 'XYl', 'XYm', 'XYn', 'XYo', 'XYp', 'XYq', 'XYr',
+			'XYs', 'XYt', 'XYu', 'XYv', 'XYw', 'XYx', 'XYy', 'XYz', 'XY',
+			'aXYZ', 'bXYZ', 'cXYZ', 'dXYZ', 'eXYZ', 'fXYZ', 'gXYZ', 'hXYZ',
+			'iXYZ', 'jXYZ', 'kXYZ', 'lXYZ', 'mXYZ', 'nXYZ', 'oXYZ', 'pXYZ',
+			'qXYZ', 'rXYZ', 'sXYZ', 'tXYZ', 'uXYZ', 'vXYZ', 'wXYZ', 'xXYZ',
+			'yXYZ', 'zXYZ', 'XaYZ', 'XbYZ', 'XcYZ', 'XdYZ', 'XeYZ', 'XfYZ',
+			'XgYZ', 'XhYZ', 'XiYZ', 'XjYZ', 'XkYZ', 'XlYZ', 'XmYZ', 'XnYZ',
+			'XoYZ', 'XpYZ', 'XqYZ', 'XrYZ', 'XsYZ', 'XtYZ', 'XuYZ', 'XvYZ',
+			'XwYZ', 'XxYZ', 'XyYZ', 'XzYZ', 'XYaZ', 'XYbZ', 'XYcZ', 'XYdZ',
+			'XYeZ', 'XYfZ', 'XYgZ', 'XYhZ', 'XYiZ', 'XYjZ', 'XYkZ', 'XYlZ',
+			'XYmZ', 'XYnZ', 'XYoZ', 'XYpZ', 'XYqZ', 'XYrZ', 'XYsZ', 'XYtZ',
+			'XYuZ', 'XYvZ', 'XYwZ', 'XYxZ', 'XYyZ', 'XYzZ', 'XYZa', 'XYZb',
+			'XYZc', 'XYZd', 'XYZe', 'XYZf', 'XYZg', 'XYZh', 'XYZi', 'XYZj',
+			'XYZk', 'XYZl', 'XYZm', 'XYZn', 'XYZo', 'XYZp', 'XYZq', 'XYZr',
+			'XYZs', 'XYZt', 'XYZu', 'XYZv', 'XYZw', 'XYZx', 'XYZy', 'XYZz'])
+		)
+
+
+def main2():
+	start = datetime.datetime.now()
+	friends = one_edit_find_friend_closures('causes')
+	print 'Took %s to find friends.' % (datetime.datetime.now() - start,)
+	print 'There are %d friends.' % (len(friends),)
+
 	
 if __name__ == '__main__':
 	#big_load()
 	#test_causes()
-	main()
+	#main()
 	#unittest.main()
+	main2()
